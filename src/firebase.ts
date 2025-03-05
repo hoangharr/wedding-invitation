c8190fe7a33d8f6a32d4b71f6c0ae92ed9845d10
Hoang Do Minh <101560502+hoangharr@users.noreply.github.com>
2025-03-05 15:31:45 +0700

    add firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
+import { getAnalytics } from 'firebase/analytics';
 
-// TODO: https://firebase.google.com/docs/web/setup?hl=ko
-// 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+  apiKey: 'AIzaSyDOuaBrONS142Oml6qW6PqYzycXNX1L0Is',
+  authDomain: 'wedding-fe3b1.firebaseapp.com',
+  projectId: 'wedding-fe3b1',
+  storageBucket: 'wedding-fe3b1.firebasestorage.app',
+  messagingSenderId: '997529710724',
+  appId: '1:997529710724:web:d258ede2e76d2fb56252d1',
+  measurementId: 'G-04V1KQ9MQ4',
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const realtimeDb = getDatabase(firebaseApp);
+export const analytics = getAnalytics(firebaseApp);
