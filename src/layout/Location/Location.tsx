dffac615d50b40994c9e8e9fc800f2a1453bcedc
Hoang Do Minh <101560502+hoangharr@users.noreply.github.com>
2025-03-06 08:24:48 +0700

    hot fix

--- src/layout/Location/Location.tsx
+++ src/layout/Location/Location.tsx
@@ -3,14 +3,14 @@ import data from 'data.json';
 import Address from './Address.tsx';
 import Map from './Map.tsx';
 import MapButtons from './MapButtons.tsx';
-import { Caption } from '@/components/Text.tsx';
+// import { Caption } from '@/components/Text.tsx';
 import { IData } from '@/types/data.ts';
 import { useLanguage } from 'context/LanguageContext';
 
 const Location = () => {
   const { language } = useLanguage();
 
-  const mapInfo = (data as IData).mapInfo[language as 'en' | 'vi'];
+  // const mapInfo = (data as IData).mapInfo[language as 'en' | 'vi'];
   return (
     <LocationWrapper>
       {/* <Caption textAlign={'center'}>{mapInfo.address1}</Caption> */}
